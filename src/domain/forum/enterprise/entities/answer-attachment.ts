1ad743e36556d9985aa767a525bd4dcd6337f5dc
rafaelsilva <todosomtodavoz@gmail.com>
2025-06-24 08:07:55 -0300

    create attachment entity

---
+++ src/domain/forum/enterprise/entities/answer-attachment.ts
@@ -0,0 +1,23 @@
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+
+
+interface AnswerAttachmentProps {
+  answerid: UniqueEntityId;
+  attachmentId: UniqueEntityId;
+}
+
+export class AnswerAttachment extends Entity<AnswerAttachmentProps> {
+  get answerId(): UniqueEntityId {
+    return this.props.answerid;
+  }
+
+  get attachmentId(): UniqueEntityId {
+    return this.props.attachmentId;
+  }
+
+   static create(props: AnswerAttachmentProps, id?: UniqueEntityId) {
+      const attachment = new AnswerAttachment(props, id);
+      return attachment;
+    }
+}
